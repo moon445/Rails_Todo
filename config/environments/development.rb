a1a0d9ad43374c8dee609f54c4bf37ff99bdbbce
EC2 Default User <ec2-user@ip-172-31-31-191.ap-northeast-1.compute.internal>
2024-07-05 06:05:53 +0000

    finish 2-3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,11 +51,15 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # devise の設定
+  config.action_mailer.default_url_options = { host: "localhost", port: ENV.fetch("PORT", 3000) }
+  
 end
